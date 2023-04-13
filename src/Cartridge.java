c4f0797c9c49908a0c763eae6aa4e8b1cb4e269d
Ethan Scrivner <escrivner03@gmail.com>
2023-04-12 22:58:36 -0500

    cartridge can initialize ROM into int array

--- src/Cartridge.java
+++ src/Cartridge.java
@@ -1,14 +1,38 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Cartridge {
     
-    int[] rom = new int[0x3FFF];
-    int[] switchableRom = new int[0x3FFF];
+    int[] rom = new int[0x7FFF+1];
 
     public Cartridge(String filename){
 
+        try {
+            
+            loadRom(filename);
+        } catch (Exception e) {
+
+            System.out.println("ROM could not be read.");
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
-    private void loadRom(String filename){
+    private void loadRom(String filename) throws IOException{
 
+        FileInputStream stream = new FileInputStream(filename);
+
+        int val = 0;
+        int pos = 0;
+
+        while((val = stream.read()) != -1){
+
+            rom[pos] = val;
+            System.out.println("index: " + pos + "\tbyte: " + val);
+            pos++;
+        }
     }
 
     public int readAddress(int address){
